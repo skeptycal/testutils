ac8ce39e3b86bcc859602f7641a4c518055f2368
Michael Treanor <skeptycal@gmail.com>
2022-02-01 12:20:21 -0600

    (GoBot) devtag version v0.1.0-1643738384413548: initial commit

---
+++ cmd/example/main.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	testutils.Example()
+}
--- utils.go
+++ utils.go
@@ -7,12 +7,15 @@ func CheckType(any Any, typ string) bool { return GetType(any) == typ }
 
 func GetType(any Any) string {
 	if any == nil {
-		return "nil"
+		return "<nil>"
 	}
 	return reflect.ValueOf(any).Type().String()
 }
 
 func remove(slice []int, i int) []int {
+	if i >= len(slice) || i < 0 {
+		return nil
+	}
 	copy(slice[i:], slice[i+1:])
 	return slice[:len(slice)-1]
 }
